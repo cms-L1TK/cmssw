1a761966b0e39c5ab881afa1f66822dc52f40d50
Jack Li <jingyan.li@cern.ch>
2021-10-16 22:04:49 +0200

    Fixing Bug

--- L1Trigger/TrackTrigger/python/ProducerHPH_cfi.py
+++ L1Trigger/TrackTrigger/python/ProducerHPH_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 HitPatternHelper_params = cms.PSet (
 
-  HPHdebug   = cms.bool(False),   
+  hphDebug   = cms.bool(False),   
   useNewKF   = cms.bool(False),
   chosenRofZ = cms.double(50.0),
   deltaTanL = cms.double(0.125)
