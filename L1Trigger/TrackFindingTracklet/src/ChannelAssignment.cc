5a0da3745ebfb72a7658a215a58e2c196bb31aa0
Thomas Schuh <thomas.schuh@stfc.ac.uk>
2021-11-11 11:57:53 +0000

    renamed TrackBuilderChannel to ChannelAssignment now also containing DTC to IR mapping

--- L1Trigger/TrackFindingTracklet/src/ChannelAssignment.cc
+++ L1Trigger/TrackFindingTracklet/src/ChannelAssignment.cc
@@ -1,29 +1,24 @@
 #include "L1Trigger/TrackFindingTracklet/interface/ChannelAssignment.h"
-#include "L1Trigger/TrackFindingTracklet/interface/Settings.h"
 
 #include <vector>
-#include <array>
-#include <set>
-#include <algorithm>
 
 using namespace std;
 using namespace edm;
 using namespace tt;
 
-namespace trklet {
+namespace trackFindingTracklet {
 
   ChannelAssignment::ChannelAssignment(const edm::ParameterSet& iConfig, const Setup* setup)
       : setup_(setup),
         useDuplicateRemoval_(iConfig.getParameter<bool>("UseDuplicateRemoval")),
         boundaries_(iConfig.getParameter<vector<double>>("PtBoundaries")),
-        seedTypeNames_(iConfig.getParameter<vector<string>>("SeedTypesReduced")),
+        seedTypeNames_(iConfig.getParameter<vector<string>>("SeedTypes")),
         numSeedTypes_(seedTypeNames_.size()),
         numChannels_(useDuplicateRemoval_ ? 2 * boundaries_.size() : numSeedTypes_),
         maxNumProjectionLayers_(iConfig.getParameter<int>("MaxNumProjectionLayers")),
-        channelEncoding_(iConfig.getParameter<vector<int>>("IRChannelsIn")) {
-    const ParameterSet& pSetSeedTypesSeedLayers = iConfig.getParameter<ParameterSet>("SeedTypesSeedLayersReduced");
-    const ParameterSet& pSetSeedTypesProjectionLayers =
-        iConfig.getParameter<ParameterSet>("SeedTypesProjectionLayersReduced");
+        channelEncoding_(iConfig.getParameter<int>("IRChannelsIn")) {
+    const ParameterSet& pSetSeedTypesSeedLayers = iConfig.getParameter<ParameterSet>("SeedTypesSeedLayers");
+    const ParameterSet& pSetSeedTypesProjectionLayers = iConfig.getParameter<ParameterSet>("SeedTypesProjectionLayers");
     seedTypesSeedLayers_.reserve(numSeedTypes_);
     seedTypesProjectionLayers_.reserve(numSeedTypes_);
     for (const string& s : seedTypeNames_) {
@@ -113,7 +108,7 @@ namespace trklet {
         for (const auto& s : seedTypeNames_)
           exception << s << " ";
         exception << ").";
-        exception.addContext("trklet:ChannelAssignment:channelId");
+        exception.addContext("trackFindingTracklet:ChannelAssignment:channelId");
         throw exception;
       }
       channelId = ttTrackRef->phiSector() * numSeedTypes_ + seedType;
@@ -134,12 +129,13 @@ namespace trklet {
     return true;
   }
 
-  // sets layerId of given TTStubRef and seedType, returns false if seeed stub
-  bool ChannelAssignment::layerId(int seedType, const TTStubRef& ttStubRef, int& layerId) {
+  // sets layerId of given TTStubRef and TTTrackRef, returns false if seeed stub
+  bool ChannelAssignment::layerId(const TTTrackRef& ttTrackRef, const TTStubRef& ttStubRef, int& layerId) {
     layerId = -1;
+    const int seedType = ttTrackRef->trackSeedType();
     if (seedType < 0 || seedType >= numSeedTypes_) {
       cms::Exception exception("logic_error");
-      exception.addContext("trklet::ChannelAssignment::layerId");
+      exception.addContext("trackFindingTracklet::ChannelAssignment::layerId");
       exception << "TTTracks with with seed type " << seedType << " not supported.";
       throw exception;
     }
@@ -152,7 +148,7 @@ namespace trklet {
     if (pos == projectingLayers.end()) {
       const string& name = seedTypeNames_[seedType];
       cms::Exception exception("logic_error");
-      exception.addContext("trklet::ChannelAssignment::layerId");
+      exception.addContext("trackFindingTracklet::ChannelAssignment::layerId");
       exception << "TTStub from layer " << layer << " (barrel: 1-6; discs: 11-15) from seed type " << name
                 << " not supported.";
       throw exception;
