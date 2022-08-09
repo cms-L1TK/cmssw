4c6d37f77a6839e136e8ddae13b67cd65730b19c
Anders <aryd@cern.ch>
2022-08-09 14:04:28 +0200

    These changes implements a LUT as function of the raidal projection in the disks to determine which r bins needs to be searched - and the finer position within the r bin

--- L1Trigger/TrackFindingTracklet/interface/TrackletLUT.h
+++ L1Trigger/TrackFindingTracklet/interface/TrackletLUT.h
@@ -79,6 +79,8 @@ namespace trklet {
 
     void initBendMatch(unsigned int layerdisk);
 
+    void initProjectionDiskRadius(int nrbits);
+
     enum VMRTableType { me, disk, inner, inneroverlap, innerthird };
 
     //region only used for name - should be removed
