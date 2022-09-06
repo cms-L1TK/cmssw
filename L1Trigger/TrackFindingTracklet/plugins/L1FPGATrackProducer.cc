3246d10e4e1e6581376534580063f70ce5605296
tschuh <thomas.schuh@stfc.ac.uk>
2022-09-06 19:27:11 +0200

    Update L1FPGATrackProducer.cc

--- L1Trigger/TrackFindingTracklet/plugins/L1FPGATrackProducer.cc
+++ L1Trigger/TrackFindingTracklet/plugins/L1FPGATrackProducer.cc
@@ -129,11 +129,12 @@ public:
   bool operator()(const trklet::L1TStub& a, const trklet::L1TStub& b) const {
     if (a.x() != b.x())
       return (b.x() > a.x());
-    if (a.y() != b.y())
+    else if (a.y() != b.y())
       return (b.y() > a.y());
-    if (a.z() != b.z())
+    else if (a.z() != b.z())
       return (a.z() > b.z());
-    return a.bend() > b.bend();
+    else
+      return a.bend() > b.bend();
   }
 };
 
