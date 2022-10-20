99e14bf927626852d173dcb0bb1233474c4bad76
Thomas Schuh <thomas.schuh@stfc.ac.uk>
2022-10-20 13:27:20 +0100

    kf bug fixes, f/w sync.

--- L1Trigger/TrackFindingTracklet/test/demonstrator_cfg.py
+++ L1Trigger/TrackFindingTracklet/test/demonstrator_cfg.py
@@ -67,7 +67,7 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(options.Even
 process.source = cms.Source(
   "PoolSource",
   fileNames = cms.untracked.vstring( options.inputMC ),
-  #skipEvents = cms.untracked.uint32( 993 ),
+  #skipEvents = cms.untracked.uint32( 5 ),
   secondaryFileNames = cms.untracked.vstring(),
   duplicateCheckMode = cms.untracked.string( 'noDuplicateCheck' )
 )
