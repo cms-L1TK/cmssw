185100fcbfe55a847177587d8a3f7c50e6940fa2
Ian Tomalin <ian.tomalin@stfc.ac.uk>
2024-03-02 02:00:12 +0000

    Updated to 14_0_0 MC

--- L1Trigger/TrackFindingTracklet/test/skimForCI_cfg.py
+++ L1Trigger/TrackFindingTracklet/test/skimForCI_cfg.py
@@ -36,8 +36,8 @@ process.load('Configuration.EventContent.EventContent_cff')
 process.load('Configuration.StandardSequences.MagneticField_cff')
 
 # D88 geometry (T24 tracker)
-process.load('Configuration.Geometry.GeometryExtended2026D88Reco_cff')
-process.load('Configuration.Geometry.GeometryExtended2026D88_cff')
+process.load('Configuration.Geometry.GeometryExtended2026D98Reco_cff')
+process.load('Configuration.Geometry.GeometryExtended2026D98_cff')
 
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 
@@ -50,12 +50,8 @@ process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1000))
 from MCsamples.Scripts.getCMSdata_cfi import *
 
 # Read data from card files (defines getCMSdataFromCards()):
-#from MCsamples.RelVal_1130_D76.PU0_TTbar_14TeV_cfi import *
-#inputMC = getCMSdataFromCards()
-
-#dataName="/RelValTTbar_14TeV/CMSSW_12_6_0_pre4-125X_mcRun4_realistic_v2_2026D88noPU-v1/GEN-SIM-DIGI-RAW"
-dataName="/RelValTTbar_14TeV/CMSSW_12_6_0-125X_mcRun4_realistic_v5_2026D88noPURV183-v1/GEN-SIM-DIGI-RAW"
-inputMC=getCMSdata(dataName)
+from MCsamples.RelVal_1400_D98.PU0_TTbar_14TeV_cfi import *
+inputMC = getCMSdataFromCards()
 
 process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(*inputMC))
 
@@ -77,8 +73,8 @@ process.output.outputCommands.append('keep  *_*_*MergedTrackTruth*_*')
 process.output.outputCommands.append('keep  *_genParticles_*_*')
 
 # Add this if you need to rereconstruct the stubs.
-process.output.outputCommands.append('keep  Phase2TrackerDigi*_mix_Tracker_*')
-process.output.outputCommands.append('keep  PixelDigiSimLinked*_simSiPixelDigis_Tracker_*')
+#process.output.outputCommands.append('keep  Phase2TrackerDigi*_mix_Tracker_*')
+#process.output.outputCommands.append('keep  PixelDigiSimLinked*_simSiPixelDigis_Tracker_*')
 
 process.pd = cms.EndPath(process.output)
 
