afa515f687229a9600c91c996d4571f0e42b6d37
Thomas Schuh <thomas.schuh@stfc.ac.uk>
2024-03-12 13:27:48 +0000

    changed default values for demonstrator link mapping.

--- L1Trigger/TrackFindingTracklet/python/Demonstrator_cfi.py
+++ L1Trigger/TrackFindingTracklet/python/Demonstrator_cfi.py
@@ -8,7 +8,7 @@ TrackTriggerDemonstrator_params = cms.PSet (
   DirIPBB  = cms.string( "/heplnw039/tschuh/work/proj/DRinDR/" ), # path to ipbb proj area
   RunTime  = cms.double( 4.5 ),                                   # runtime in us
 
-  LinkMappingIn  = cms.vint32( 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51 ),
-  LinkMappingOut = cms.vint32( 52, 53, 54, 55, 56, 57, 58, 59, 60 )
+  LinkMappingIn  = cms.vint32(  ),
+  LinkMappingOut = cms.vint32(  )
 
 )
