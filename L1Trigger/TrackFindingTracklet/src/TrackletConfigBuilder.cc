6f6d99e484f461af8db0637ff8645040bdf5e140
mlarson <mark.steven.larson@cern.ch>
2024-05-15 23:41:09 +0200

    VMSMERouter calculating VMStubs

--- L1Trigger/TrackFindingTracklet/src/TrackletConfigBuilder.cc
+++ L1Trigger/TrackFindingTracklet/src/TrackletConfigBuilder.cc
@@ -1106,9 +1106,6 @@ void TrackletConfigBuilder::writeVMSMemories(std::ostream& os, std::ostream& mem
       for (unsigned int iReg = 0; iReg < NRegions_[ilayer]; iReg++) {
         memories << "VMStubsME: VMSME_" << LayerName(ilayer) << "PHI" << iTCStr(iReg) << "n1 [18]" << std::endl;
         memories << "VMStubsME: VMSME_" << LayerName(ilayer) << "PHI" << iTCStr(iReg) << "n2 [18]" << std::endl;
-        os << "VMSME_" << LayerName(ilayer) << "PHI" << iTCStr(iReg) << "n1"
-           << " input=> VMR_" << LayerName(ilayer) << "PHI" << iTCStr(iReg) << ".vmstuboutPHI" << iTCStr(iReg)
-           << " output=> VMSMER_" << LayerName(ilayer) << "PHI" << iTCStr(iReg) << ".vmstubin" << std::endl;
         os << "VMSME_" << LayerName(ilayer) << "PHI" << iTCStr(iReg) << "n2"
            << " input=> VMSMER_" << LayerName(ilayer) << "PHI" << iTCStr(iReg) << ".vmstubout"
            << " output=> MP_" << LayerName(ilayer) << "PHI" << iTCStr(iReg) << ".vmstubin" << std::endl;
