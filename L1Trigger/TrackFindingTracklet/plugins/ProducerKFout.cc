e5fd301b8fba362eb4fd6046fbeac4a43df4bf4e
cgsavard <claire.savard@colorado.edu>
2024-05-17 12:47:12 -0600

    Bin TQ MVA output (#272)
    * add TQ MVA output binning scheme
    
    * remove code no longer used
    
    * move logistic sigmoid into TTTrack class
    
    * fix for proper precision after MVA conversion in track word
    
    * label all MVA variables before logistic sigmoid with 'Pre'
    
    * formatting
    
    * fix filling of track word before running TQ MVA
    
    * switch to post-sigmoid mva bins
    
    * remove any onnx instances
    
    * fix MVA1 initialization
    
    * bin TQ MVA in pre-sigmoided bins in KFout
    
    * move pre-sigmoid bins to track quality class
    
    * change minimum bin

--- L1Trigger/TrackFindingTracklet/plugins/ProducerKFout.cc
+++ L1Trigger/TrackFindingTracklet/plugins/ProducerKFout.cc
@@ -68,7 +68,6 @@ namespace trklet {
     vector<double> dZBins_;
 
     std::unique_ptr<L1TrackQuality> trackQualityModel_;
-    vector<int> tqBins_;
     double tqTanlScale_;
     double tqZ0Scale_;
     static constexpr double ap_fixed_rescale = 32.0;
@@ -78,13 +77,14 @@ namespace trklet {
 
     int partialTrackWordBits_;
 
-    // Helper function to convert floating chi2 to chi2 bin
+    // Helper function to convert floating value to bin
     template <typename T>
     unsigned int digitise(const T& bins, double value, double factor) {
       unsigned int bin = 0;
       for (unsigned int i = 0; i < bins.size() - 1; i++) {
         if (value * factor > bins[i] && value * factor <= bins[i + 1])
-          bin = i;
+          break;
+        bin++;
       }
       return bin;
     }
@@ -111,7 +111,6 @@ namespace trklet {
 
     trackQualityModel_ = std::make_unique<L1TrackQuality>(iConfig.getParameter<edm::ParameterSet>("TrackQualityPSet"));
     edm::ParameterSet trackQualityPSset = iConfig.getParameter<edm::ParameterSet>("TrackQualityPSet");
-    tqBins_ = trackQualityPSset.getParameter<vector<int>>("tqemu_bins");
     tqTanlScale_ = trackQualityPSset.getParameter<double>("tqemu_TanlScale");
     tqZ0Scale_ = trackQualityPSset.getParameter<double>("tqemu_Z0Scale");
   }
@@ -200,7 +199,7 @@ namespace trklet {
       StreamsTrack outputStreamsTracks(setup_->numRegions() * setup_->tfpNumChannel());
 
       // Setup containers for track quality
-      float tempTQMVA = 0.0;
+      float tempTQMVAPreSig = 0.0;
       // Due to ap_fixed implementation in CMSSW this 10,5 must be specified at compile time, TODO make this a changeable parameter
       std::vector<ap_fixed<10, 5>> trackQuality_inputs = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 
@@ -295,10 +294,11 @@ namespace trklet {
               digitise(TTTrack_TrackWord::chi2RZBins, tempchi2rz, (double)setup_->kfoutchi2rzConv())};
 
           // Run BDT emulation and package output into 3 bits
-
-          tempTQMVA = trackQualityModel_->runEmulatedTQ(trackQuality_inputs);
-          tempTQMVA = std::trunc(tempTQMVA * ap_fixed_rescale);
-          TTBV tqMVA(digitise(tqBins_, tempTQMVA, 1.0), TTTrack_TrackWord::TrackBitWidths::kMVAQualitySize, false);
+          // output needs sigmoid transformation applied
+          tempTQMVAPreSig = trackQualityModel_->runEmulatedTQ(trackQuality_inputs);
+          TTBV tqMVA(digitise(L1TrackQuality::getTqMVAPreSigBins(), tempTQMVAPreSig, 1.0),
+                     TTTrack_TrackWord::TrackBitWidths::kMVAQualitySize,
+                     false);
 
           // Build 32 bit partial tracks for outputting in 64 bit packets
           //                  12 +  3       +  7         +  3    +  6
