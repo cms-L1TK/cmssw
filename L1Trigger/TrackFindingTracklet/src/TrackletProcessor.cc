02276dc9f83c4a0665133374d800756002390e33
mlarson02 <123397857+mlarson02@users.noreply.github.com>
2024-06-07 09:26:22 -0400

    Removing unneeded + 1 to rbits in TP

--- L1Trigger/TrackFindingTracklet/src/TrackletProcessor.cc
+++ L1Trigger/TrackFindingTracklet/src/TrackletProcessor.cc
@@ -408,7 +408,7 @@ void TrackletProcessor::execute(unsigned int iSector, double phimin, double phim
             (stub->r().nbits() + 1 -
              nbitsrfinebintable_);  // + 1 required to offset artificial decrease in # of diskps r bits from 12 -> 11 to make space for negDisk bit
       } else {  //Take the top nbitsfinebintable_ bits of the z coordinate
-        indexr = (stub->rvalue() >> (stub->r().nbits() + 1 - nbitsrfinebintable_)) & ((1 << nbitsrfinebintable_) - 1);
+        indexr = (stub->rvalue() >> (stub->r().nbits() - nbitsrfinebintable_)) & ((1 << nbitsrfinebintable_) - 1);
       }
 
       int lutval = -1;
