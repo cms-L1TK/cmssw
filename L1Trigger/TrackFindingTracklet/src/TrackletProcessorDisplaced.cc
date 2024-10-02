63d6b65780ebc5929cacef3b5bc7250f9676332e
Claire <claire.savard@colorado.edu>
2024-10-02 13:09:51 -0500

    include truncation in TrackletProcessorDisplaced

--- L1Trigger/TrackFindingTracklet/src/TrackletProcessorDisplaced.cc
+++ L1Trigger/TrackFindingTracklet/src/TrackletProcessorDisplaced.cc
@@ -427,12 +427,18 @@ void TrackletProcessorDisplaced::execute(unsigned int iSector, double phimin, do
                       if (settings_.debugTracklet()) {
                         edm::LogVerbatim("Tracklet") << "TrackletCalculatorDisplaced execute done";
                       }
+		      if (countall >= settings_.maxStep("TP")) break;
                     }
+		    if (countall >= settings_.maxStep("TP")) break;
                   }
+		  if (countall >= settings_.maxStep("TP")) break;
                 }
               }
+	      if (countall >= settings_.maxStep("TP")) break;
             }
+	    if (countall >= settings_.maxStep("TP")) break;
           }
+	  if (countall >= settings_.maxStep("TP")) break;
         }
 
       } else if (layer1_ == 2 && layer2_ == 3) {
@@ -540,12 +546,18 @@ void TrackletProcessorDisplaced::execute(unsigned int iSector, double phimin, do
                       if (settings_.debugTracklet()) {
                         edm::LogVerbatim("Tracklet") << "TrackletCalculatorDisplaced execute done";
                       }
+		      if (countall >= settings_.maxStep("TP")) break;
                     }
+		    if (countall >= settings_.maxStep("TP")) break;
                   }
+		  if (countall >= settings_.maxStep("TP")) break;
                 }
               }
+	      if (countall >= settings_.maxStep("TP")) break;
             }
+	    if (countall >= settings_.maxStep("TP")) break;
           }
+	  if (countall >= settings_.maxStep("TP")) break;
         }
 
       } else if (disk1_ == 1 && disk2_ == 2) {
@@ -654,15 +666,23 @@ void TrackletProcessorDisplaced::execute(unsigned int iSector, double phimin, do
                       if (settings_.debugTracklet()) {
                         edm::LogVerbatim("Tracklet") << "TrackletCalculatorDisplaced execute done";
                       }
+		      if (countall >= settings_.maxStep("TP")) break;
                     }
+		    if (countall >= settings_.maxStep("TP")) break;
                   }
+		  if (countall >= settings_.maxStep("TP")) break;
                 }
               }
+	      if (countall >= settings_.maxStep("TP")) break;
             }
+	    if (countall >= settings_.maxStep("TP")) break;
           }
+	  if (countall >= settings_.maxStep("TP")) break;
         }
       }
+      if (countall >= settings_.maxStep("TP")) break;
     }
+    if (countall >= settings_.maxStep("TP")) break;
   }
 
   if (settings_.writeMonitorData("TPD")) {
