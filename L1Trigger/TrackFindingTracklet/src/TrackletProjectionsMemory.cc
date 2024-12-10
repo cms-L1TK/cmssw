a1ffc41bff3e53a8321e4bcf30578311597971cf
Anders <aryd@cern.ch>
2024-12-10 17:15:15 +0100

    Code additions to write the TPAR memmories from the MPAR memories

--- L1Trigger/TrackFindingTracklet/src/TrackletProjectionsMemory.cc
+++ L1Trigger/TrackFindingTracklet/src/TrackletProjectionsMemory.cc
@@ -49,6 +49,10 @@ void TrackletProjectionsMemory::writeTPROJ(bool first, unsigned int iSector) {
   iSector_ = iSector;
   const string dirTP = settings_.memPath() + "TrackletProjections/";
 
+
+  //Hack to suppress writing empty TPROJ memories - only want to write MPROJ memories
+  if (getName()[0] == 'T') return;
+  
   std::ostringstream oss;
   oss << dirTP << "TrackletProjections_" << getName() << "_" << std::setfill('0') << std::setw(2) << (iSector_ + 1)
       << ".dat";
@@ -59,12 +63,27 @@ void TrackletProjectionsMemory::writeTPROJ(bool first, unsigned int iSector) {
   out_ << "BX = " << (bitset<3>)bx_ << " Event : " << event_ << endl;
 
   for (unsigned int j = 0; j < tracklets_.size(); j++) {
+    // This is a hack here to write out the TPAR files for backward compatibility
+    std::ofstream out;
+    std::string moduleName = getName().substr(0,10);;
+    moduleName[0]='T';
+    std::ostringstream oss2;
+    char postfix = getName()[10];
+    postfix+=j;
+    oss2 << dirTP << "TrackletProjections_" << moduleName << postfix << "_" << getName().substr(getName().size()-6,6)
+	 << "_" <<std::setfill('0') << std::setw(2) << (iSector_ + 1)
+	 << ".dat";
+    std::string fnameTPAR = oss2.str();
+    openfile(out, first, dirTP, fnameTPAR, __FILE__, __LINE__);
+    out << "BX = " << (bitset<3>)bx_ << " Event : " << event_ << endl;
     for (unsigned int i = 0; i < tracklets_[j].size(); i++) {
     
       string proj = (layer_ > 0 && tracklets_[j][i]->validProj(layer_ - 1)) ? tracklets_[j][i]->trackletprojstrlayer(layer_)
 	: tracklets_[j][i]->trackletprojstrdisk(disk_);
       out_ << hexstr(j) << " " << hexstr(i) << " " << proj << "  " << trklet::hexFormat(proj) << endl;
+      out << hexstr(i) << " " << proj << "  " << trklet::hexFormat(proj) << endl;
     }
+    out.close();
   }
   out_.close();
 
