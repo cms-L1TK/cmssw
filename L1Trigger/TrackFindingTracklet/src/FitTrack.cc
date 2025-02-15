f296f03e4442be8678da3e21cd479cc7926748ce
Anders <aryd@cern.ch>
2025-02-15 19:30:13 +0100

    Fix radial position

--- L1Trigger/TrackFindingTracklet/src/FitTrack.cc
+++ L1Trigger/TrackFindingTracklet/src/FitTrack.cc
@@ -1000,13 +1000,13 @@ void FitTrack::execute(deque<string>& streamTrackRaw,
             r = string(widthDisk2Sidentifier, '0') + r;
             //std::cout << "2s stub : " << r << std::endl;
           }
-          //bool diskPS = (stub->disk() != 0) && (stub->isPSmodule() != 0);
-          //if (diskPS) {
-          //  std::cout << "old r: " << r << " new r: " ;
-          //  FPGAWord tmp(resid.stubptr()->rvalue(), 12);
-          //  r = tmp.str();
-          //  std::cout << r << std::endl;
-          //}
+          bool diskPS = (stub->disk() != 0) && (stub->isPSmodule() != 0);
+          if (diskPS) {
+            //  std::cout << "old r: " << r << " new r: " ;
+            FPGAWord tmp(resid.stubptr()->rvalue(), 12);
+            r = tmp.str();
+            //  std::cout << r << std::endl;
+          }
           const string& stubId = resid.fpgastubid().str();
           // store seed, L1TStub, and bit accurate 64 bit word in clock accurate output
           streamsStubRaw[ihit++].emplace_back(seedType, *stub, valid + stubId + r + phi + rz);
