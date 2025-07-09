80af412291ceddccfd268832f4f33ac1c3660c45
Alaa A. Abdelhamid <130788379+A-A-Abdelhamid@users.noreply.github.com>
2025-07-09 15:23:12 -0400

    Update ChannelAssignment_cfi.py

--- L1Trigger/TrackFindingTracklet/python/ChannelAssignment_cfi.py
+++ L1Trigger/TrackFindingTracklet/python/ChannelAssignment_cfi.py
@@ -8,8 +8,8 @@ ChannelAssignment_params = cms.PSet (
   TM = cms.PSet (
     # The order here approximately dictates the order in which tracks enter the DR,
     # with DR keeping 1st track to arrive, in case two tracks are duplicate.
-    MuxOrder    = cms.vstring( "L1L2", "L2L3", "L1D1", "D1D2", "D3D4", "L2D1", "L3L4", "L5L6" ),
-    NumLayers   = cms.int32( 11 ), # number of layers per track
+    MuxOrder    = cms.vstring( "L1L2", "L2L3", "L1D1", "D1D2", "D3D4", "L2D1", "L3L4", "L5L6", "L3L4L2","L5L6L4", "L2L3D1", "D1D2L2" ),
+    NumLayers   = cms.int32( 15 ), # number of layers per track
     WidthStubId = cms.int32( 10 ), # number of bits used to represent stub id for projected stubs
     WidthCot    = cms.int32( 14 )
   ),
@@ -20,7 +20,7 @@ ChannelAssignment_params = cms.PSet (
     MinIdenticalStubs    = cms.int32(  3 )  # min number of shared stubs to identify duplicates
   ),
 
-  SeedTypes = cms.vstring( "L1L2", "L2L3", "L3L4", "L5L6", "D1D2", "D3D4", "L1D1", "L2D1" ), # seed types used in tracklet algorithm (position gives int value)
+  SeedTypes = cms.vstring( "L1L2", "L2L3", "L3L4", "L5L6", "D1D2", "D3D4", "L1D1", "L2D1" , "L3L4L2","L5L6L4", "L2L3D1", "D1D2L2" ), # seed types used in tracklet algorithm (position gives int value)
 
   SeedTypesSeedLayers = cms.PSet (      # seeding layers of seed types using default layer id [barrel: 1-6, discs: 11-15]
     L1L2 = cms.vint32(  1,  2 ),
@@ -30,7 +30,11 @@ ChannelAssignment_params = cms.PSet (
     D1D2 = cms.vint32( 11, 12 ),
     D3D4 = cms.vint32( 13, 14 ),
     L1D1 = cms.vint32(  1, 11 ),
-    L2D1 = cms.vint32(  2, 11 )
+    L2D1 = cms.vint32(  2, 11 ),
+    L3L4L2 = cms.vint32(  3, 4,  2 ),
+    L5L6L4 = cms.vint32(  5, 6,  4 ),
+    L2L3D1 = cms.vint32(  2, 3, 11 ),
+    D1D2L2 = cms.vint32( 11, 12,  2 )
   ),
   SeedTypesProjectionLayers = cms.PSet ( # layers a seed types can project to using default layer id [barrel: 1-6, discs: 11-15]
     L1L2 = cms.vint32(  3,  4,  5,  6, 11, 12, 13, 14 ),
@@ -40,7 +44,11 @@ ChannelAssignment_params = cms.PSet (
     D1D2 = cms.vint32(  1,  2, 13, 14, 15 ),
     D3D4 = cms.vint32(  1, 11, 12, 15 ),
     L1D1 = cms.vint32( 12, 13, 14, 15 ),
-    L2D1 = cms.vint32(  1, 12, 13, 14 )
+    L2D1 = cms.vint32(  1, 12, 13, 14 ),
+    L3L4L2 = cms.vint32(  1,  5,  6, 11, 12, 13 ),
+    L5L6L4 = cms.vint32(  1,  2,  3 ),
+    L2L3D1 = cms.vint32(  1, 12, 13, 14 ),
+    D1D2L2 = cms.vint32(  1, 13, 14 )
   ),
 
   IRChannelsIn = cms.vint32( range(0, 48) ) # vector of DTC id indexed by connected IR module id (from order in processingmodules.dat)
