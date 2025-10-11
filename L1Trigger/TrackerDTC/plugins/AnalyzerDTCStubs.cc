9054b563fd5c2ca1a1f7ac72da8850572a79893a
am2023 <a.mastronikolis23@imperial.ac.uk>
2025-10-11 16:46:07 +0100

    analyzer dtc stubs dumps a simple root file with occs on dtc id = 0

--- L1Trigger/TrackerDTC/plugins/AnalyzerDTCStubs.cc
+++ L1Trigger/TrackerDTC/plugins/AnalyzerDTCStubs.cc
@@ -12,10 +12,19 @@
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "DataFormats/Common/interface/Handle.h"
 
+// ROOT file service
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+
 // TTStubDetSetVec + related typedefs
 #include "DataFormats/L1TrackTrigger/interface/TTTypes.h"
-// tt::Setup
+// DetId
+#include "DataFormats/DetId/interface/DetId.h"
+// tt::Setup & SensorModule
 #include "L1Trigger/TrackTrigger/interface/Setup.h"
+#include "L1Trigger/TrackTrigger/interface/SensorModule.h"
+
+#include <vector>
 
 namespace trackerDTC 
 {
@@ -38,15 +47,19 @@ namespace trackerDTC
         const edm::InputTag inputTag_;
         edm::EDGetTokenT<TTStubDetSetVec> edGetTokenTTStubs_;
 
-        // Setup ES token
+        // Setup ES token (Event transition, used in analyze)
         edm::ESGetToken<tt::Setup, tt::SetupRcd> esGetTokenSetup_;
+
+        // Histogram: number of stubs on DTC 0 per event
+        TH1F* hisDTC0Stubs_{nullptr};
   };
 
   AnalyzerDTCStubs::AnalyzerDTCStubs(const edm::ParameterSet& iConfig)
       : inputTag_(iConfig.getParameter<edm::InputTag>("InputTag")) 
   {
+    usesResource("TFileService");  // we’ll book a histogram
     edGetTokenTTStubs_ = consumes<TTStubDetSetVec>(inputTag_);
-    esGetTokenSetup_   = esConsumes();
+    esGetTokenSetup_   = esConsumes();  // default = Event (used in analyze)
 
     LogDebug("AnalyzerDTCStubs") << "Constructed with InputTag: " << inputTag_;
   }
@@ -54,16 +67,26 @@ namespace trackerDTC
   void AnalyzerDTCStubs::beginJob() 
   {
     LogDebug("AnalyzerDTCStubs") << "beginJob()";
+
+    // Book histogram once
+    edm::Service<TFileService> fs;
+    // Choose reasonable range; adjust later if you see saturation.
+    // Here: range 0..4096 with 256 bins (like example style: maxOcc/16 bins).
+    const int    maxOcc = 2000;
+    const int    nBins  = 100;
+    const double lo     = -0.5;
+    const double hi     = maxOcc - 0.5;
+
+    hisDTC0Stubs_ = fs->make<TH1F>("HisDTC0StubOccupancy",
+                                   "DTC 0 Stub Occupancy;# stubs in DTC 0 per event;Events",
+                                   nBins, lo, hi);
   }
 
   void AnalyzerDTCStubs::beginRun(const edm::Run& iRun, const edm::EventSetup& iSetup) 
   {
     LogDebug("AnalyzerDTCStubs") << "beginRun(): run=" << iRun.run();
-
-    // Fetch Setup from EventSetup
-    // LogDebug("AnalyzerDTCStubs") << "Setup retrieved. numDTCs=" << setup.numDTCs();
-
     (void)iRun;
+    (void)iSetup;
   }
 
   void AnalyzerDTCStubs::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) 
@@ -71,39 +94,52 @@ namespace trackerDTC
     LogDebug("AnalyzerDTCStubs") << "analyze(): event=" << iEvent.id().event()
                                  << " run=" << iEvent.id().run()
                                  << " lumi=" << iEvent.id().luminosityBlock();
-    (void)iSetup;
 
-    edm::Handle<TTStubDetSetVec> handleTTStubs;
-    iEvent.getByToken(edGetTokenTTStubs_, handleTTStubs);
+    // Fetch setup (Event transition)
+    const tt::Setup& setup      = iSetup.getData(esGetTokenSetup_);
+    const int number_of_dtcs    = setup.numDTCs();
+    const int number_of_modules = setup.numModulesPerDTC();
 
-    if (!handleTTStubs.isValid()) 
+    // Fetch TTStubDetSetVec
+    edm::Handle<TTStubDetSetVec> handle;
+    iEvent.getByToken(edGetTokenTTStubs_, handle);
+
+    if (!handle.isValid()) 
     {
       edm::LogError("AnalyzerDTCStubs")
           << "Failed to get TTStubDetSetVec with InputTag " << inputTag_;
       return;
     }
 
-    const tt::Setup& setup      = iSetup.getData(esGetTokenSetup_);
-    const int number_of_dtcs    = setup.numDTCs();
-    const int number_of_modules = setup.numModulesPerDTC();
-
+    // Reorganize stubs into [DTC][module] containers
     std::vector<std::vector<std::vector<TTStubRef>>> stubsDTCs(
-    number_of_dtcs, std::vector<std::vector<TTStubRef>>(number_of_modules));
+        number_of_dtcs, std::vector<std::vector<TTStubRef>>(number_of_modules));
 
-    edm::Handle<TTStubDetSetVec> handle;
-    iEvent.getByToken(edGetTokenTTStubs_, handle);
-
-    for (auto module = handle->begin(); module != handle->end(); module++) 
+    for (auto module = handle->begin(); module != handle->end(); ++module) 
     {
+        // DetSetVec->detId + 1 = tk layout det id (per ProducerDTC pattern)
         const DetId detId = module->detId() + setup.offsetDetIdDSV();
         tt::SensorModule* sm = setup.sensorModule(detId);
+
         std::vector<TTStubRef>& stubsModule = stubsDTCs[sm->dtcId()][sm->modId()];
         stubsModule.reserve(module->size());
-        for (TTStubDetSet::const_iterator ttStub = module->begin(); ttStub != module->end(); ttStub++)
+
+        for (TTStubDetSet::const_iterator ttStub = module->begin(); ttStub != module->end(); ++ttStub)
             stubsModule.emplace_back(makeRefTo(handle, ttStub));
     }
 
-    LogDebug("AnalyzerDTCStubs") << "Retrieved TTStubDetSetVec with " << handleTTStubs->size() << " DetSets.";
+    // Count stubs in DTC 0 across all its modules
+    int nStubsDTC0 = 0;
+    if (!stubsDTCs.empty()) {
+      const auto& dtc0 = stubsDTCs.at(0);
+      for (const auto& modVec : dtc0)
+        nStubsDTC0 += static_cast<int>(modVec.size());
+    }
+
+    if (hisDTC0Stubs_) hisDTC0Stubs_->Fill(nStubsDTC0);
+
+    LogDebug("AnalyzerDTCStubs") << "DTC 0 has " << nStubsDTC0
+                                 << " stubs in this event; DetSets in input = " << handle->size();
   }
 
   void AnalyzerDTCStubs::endRun(const edm::Run& iRun, const edm::EventSetup& iSetup) 
