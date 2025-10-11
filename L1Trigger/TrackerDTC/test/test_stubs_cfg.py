9b11e13959a006f5ee161aba54619d5b3ea55b4c
am2023 <a.mastronikolis23@imperial.ac.uk>
2025-10-11 17:34:05 +0100

    added more stats

--- L1Trigger/TrackerDTC/test/test_stubs_cfg.py
+++ L1Trigger/TrackerDTC/test/test_stubs_cfg.py
@@ -13,7 +13,10 @@ process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic', '')
 
 process.load('L1Trigger.TrackerDTC.AnalyzerDTCStubs_cff')
 
-Sample = ["/store/relval/CMSSW_15_1_0_pre5/RelValTTbar_14TeV_TuneCP5/GEN-SIM-DIGI-RAW/PU_150X_mcRun4_realistic_v1_RV269_Run4D110_PU-v2/2590000/0f0bcfd3-dafe-4dda-8d39-9765f6eae68e.root"]
+Sample = ["/store/relval/CMSSW_15_1_0_pre5/RelValTTbar_14TeV_TuneCP5/GEN-SIM-DIGI-RAW/PU_150X_mcRun4_realistic_v1_RV269_Run4D110_PU-v2/2590000/0f0bcfd3-dafe-4dda-8d39-9765f6eae68e.root",
+          "/store/relval/CMSSW_15_1_0_pre3/RelValTTbar_14TeV/GEN-SIM-DIGI-RAW/PU_150X_mcRun4_realistic_v1_STD_Run4D110_PU-v1/2590000/cb282e49-ab69-4449-8d93-c465ecffe707.root",
+          "/store/relval/CMSSW_15_1_0_pre3/RelValTTbar_14TeV/GEN-SIM-DIGI-RAW/PU_150X_mcRun4_realistic_v1_STD_Run4D110_PU-v1/2590000/ddee2a64-84a8-4ca7-b4f1-5df00da1b669.root",
+          "/store/relval/CMSSW_15_1_0_pre3/RelValTTbar_14TeV/GEN-SIM-DIGI-RAW/PU_150X_mcRun4_realistic_v1_STD_Run4D110_PU-v1/2590000/37bac9a7-5c2c-4d29-9bc9-2ed5d429bdda.root"]
 
 process.source = cms.Source(
   "PoolSource",
