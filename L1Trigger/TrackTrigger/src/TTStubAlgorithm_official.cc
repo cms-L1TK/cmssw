d95636964b5e2082a3a405481a3d1b262b42ff86
Lisa Juckett <ljuckett@cern.ch>
2026-03-23 16:45:50 +0100

    Stubs calculated from straight up, not z=0

--- L1Trigger/TrackTrigger/src/TTStubAlgorithm_official.cc
+++ L1Trigger/TrackTrigger/src/TTStubAlgorithm_official.cc
@@ -67,14 +67,14 @@ void TTStubAlgorithm_official<Ref_Phase2TrackerDigi_>::PatternHitCorrelation(
   double alpha;
   double delta;
   // In case of cosmics, calcualtions use Y instead of R.
-  // delta is calculated from Y = 1500cm to simulate straight tracks from above
+  // delta is calculated from Y = 1500cm and with Z = Z0 to simulate straight tracks from above
   // Otherwise "collision point" is at x = 0 y = 0 z = 0
   if (mCosmics) {
     double Y0 = det0->position().y();
     double Y1 = det1->position().y();
     double DY = Y1 - Y0;
     alpha = atan2(DY, DZ);
-    delta = sqrt(DY * DY + DZ * DZ) / ((Y0 - 1500) * sin(alpha) + Z0 * cos(alpha));
+    delta = sqrt(DY * DY + DZ * DZ) / ((Y0 - 1500) * sin(alpha));
   } else {
     alpha = atan2(DR, DZ);
     delta = sqrt(DR * DR + DZ * DZ) / (R0 * sin(alpha) + Z0 * cos(alpha));
