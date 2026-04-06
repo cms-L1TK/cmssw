9b55eb22bd6d8107ecd2bb2a79526aeb4e1deafd
Alaa A. Abdelhamid <130788379+A-A-Abdelhamid@users.noreply.github.com>
2026-04-06 15:40:33 -0400

    Modify L1TrackNtuplePlot parameters

--- L1Trigger/TrackFindingTracklet/test/L1TrackNtuplePlot.C
+++ L1Trigger/TrackFindingTracklet/test/L1TrackNtuplePlot.C
@@ -46,11 +46,11 @@ void makeAllAndTruePDF(TString type, TString dir, TString variable, TH1F* h_all,
 
 void L1TrackNtuplePlot(TString inputRootFile = "L1TrkNtuple",
                        TString inputDir = "./",
-                       bool useDisplacedTrkCuts = false,
+                       bool useDisplacedTrkCuts = true,
                        bool doDetailedPlots = true,
                        // For displaced tracking studies, consider using TP_minPt = 3.0, TP_maxEta = 2.0.
-                       float TP_minPt = 2.0,
-                       float TP_maxEta = 2.5,
+                       float TP_minPt = 3.0,
+                       float TP_maxEta = 2.0,
                        // The following four options may not work for all plots ...
                        int TP_select_injet = 0,
                        int TP_select_pdgid = 0,
