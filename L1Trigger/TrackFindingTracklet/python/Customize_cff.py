ab8734344fab80abc7f27ab3432ea9cdc2ec158c
Alaa A. Abdelhamid <130788379+A-A-Abdelhamid@users.noreply.github.com>
2026-04-08 12:26:27 -0400

    Add NumComparisonModules to ChannelAssignment

--- L1Trigger/TrackFindingTracklet/python/Customize_cff.py
+++ L1Trigger/TrackFindingTracklet/python/Customize_cff.py
@@ -45,6 +45,7 @@ def displacedNewKFKillConfig(process):
   process.TrackTriggerSetup.Firmware.EnableTruncation = False
   process.l1tTTTracksFromExtendedTrackletEmulation.Fakefit = True
   process.l1tTTTracksFromExtendedTrackletEmulation.DoMultipleMatches = False
+  process.ChannelAssignment.DR.NumComparisonModules = cms.int32(9999) # number of comparison modules used in each DR node
   process.ChannelAssignment.SeedTypes = ( "L1L2", "L2L3", "L3L4", "L5L6", "D1D2", "D3D4", "L1D1", "L2D1", "L2L3L4", "L4L5L6", "L2L3D1", "D1D2L2" )
   process.ChannelAssignment.TM.MuxOrder = ( "L1L2", "L3L4", "D3D4", "D1D2", "L2L3", "L2D1", "L5L6", "L1D1", "L2L3L4",  "L4L5L6", "D1D2L2" , "L2L3D1" )
   process.ChannelAssignment.SeedTypesSeedLayers = cms.PSet (
