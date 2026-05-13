c2ffd400a0f44f7d7cddfa66e759ff7f5febc4ee
Ian Tomalin <ian.tomalin@stfc.ac.uk>
2026-05-13 18:25:02 +0100

    Updated to work with CMSSW 15

--- L1Trigger/TrackTrigger/test/AnalyzerClusterStub_cfg.py
+++ L1Trigger/TrackTrigger/test/AnalyzerClusterStub_cfg.py
@@ -2,78 +2,74 @@
 # SLHCUpgradeSimulations                           #
 # Configuration file for Full Workflow             #
 # Step 2 (again)                                   #
-# Understand if everything is fine with            #
-# L1TkCluster e L1TkStub                           #
+# Validate the L1 track trigger inputs             #
+# TTCluster & TTStub.                              #
 ####################################################
-# Nicola Pozzobon                                  #
+# Author: Nicola Pozzobon                          #
 # CERN, August 2012                                #
+# Updated: Ian Tomalin                             #
+# RAL, May 2026                                    #
 ####################################################
 
-#################################################################################################
+################################################################################
 # import of general framework
-#################################################################################################
+################################################################################
 import FWCore.ParameterSet.Config as cms
-#--------------import os
+
 process = cms.Process('AnalyzerClusterStub')
 
-#################################################################################################
+GEOMETRY = "D110"
+
+################################################################################
 # global tag
-#################################################################################################
+################################################################################
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgradePLS3', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic', '')
 
-#################################################################################################
+################################################################################
 # load the specific tracker geometry
-#################################################################################################
-process.load('Configuration.Geometry.GeometryExtendedPhase2TkBE5DReco_cff')
-process.load('Configuration.Geometry.GeometryExtendedPhase2TkBE5D_cff')
-process.load('Geometry.TrackerGeometryBuilder.StackedTrackerGeometry_cfi')
+################################################################################
+process.load('Configuration.Geometry.GeometryExtendedRun4' + GEOMETRY + 'Reco_cff')
+process.load('Configuration.Geometry.GeometryExtendedRun4' + GEOMETRY +'_cff')
 
-#################################################################################################
+################################################################################
 # load the magnetic field
-#################################################################################################
+################################################################################
 process.load('Configuration.StandardSequences.MagneticField_cff')
 
-#################################################################################################
+################################################################################
 # define the source and maximum number of events to generate and simulate
-#################################################################################################
+################################################################################
 process.source = cms.Source("PoolSource",
-    secondaryFileNames = cms.untracked.vstring(),
-     fileNames = cms.untracked.vstring('file:TenMuPt_0_20_ExtendedPhase2TkBE5D_10000_DIGI_L1_DIGI2RAW_L1TT_RECO.root')
-#     fileNames = cms.untracked.vstring('file:TenMuPt_0_20_ExtendedPhase2TkBE5D_10000_DIGI_L1_DIGI2RAW_L1TT_RECO_140PU.root')
-#     fileNames = cms.untracked.vstring('file:test.root')
-
-#     fileNames = cms.untracked.vstring('file:TenMuPt_2_ExtendedPhase2TkBE5D_5000_DIGI_L1_DIGI2RAW_L1TT_RECO.root',
-#                                       #'file:TenMuPt_10_ExtendedPhase2TkBE5D_5000_DIGI_L1_DIGI2RAW_L1TT_RECO.root',
-#                                       #'file:TenMuPt_100_ExtendedPhase2TkBE5D_5000_DIGI_L1_DIGI2RAW_L1TT_RECO.root'
-#     )
-
-
-#     fileNames = cms.untracked.vstring('file:TenPiPt_0_50_ExtendedPhase2TkBE5D_5000_DIGI_L1_DIGI2RAW_L1TT_RECO.root')
-#     fileNames = cms.untracked.vstring('file:TenElePt_0_50_ExtendedPhase2TkBE5D_5000_DIGI_L1_DIGI2RAW_L1TT_RECO.root')
-#     fileNames = cms.untracked.vstring('file:DYTauTau_ExtendedPhase2TkBE5D_750_DIGI_L1_DIGI2RAW_L1TT_RECO.root')
+    #fileNames = cms.untracked.vstring('/store/relval/CMSSW_15_1_0_pre5/RelValDoubleMuFlatPt1p5To8/GEN-SIM-DIGI-RAW/150X_mcRun4_realistic_v1_RV269_Run4D110_noPU-v1/2590000/1172421f-823f-420f-8ec9-3de20dd6dda4.root')
+    #fileNames = cms.untracked.vstring('/store/relval/CMSSW_15_1_0_pre5/RelValTTbar_14TeV_TuneCP5/GEN-SIM-DIGI-RAW/150X_mcRun4_realistic_v1_RV269_Run4D110_noPU-v1/2590000/02a7204c-3f1c-4858-9350-2f506d729bbc.root')
+    fileNames = cms.untracked.vstring('/store/relval/CMSSW_15_1_0_pre5/RelValTTbar_14TeV_TuneCP5/GEN-SIM-DIGI-RAW/PU_150X_mcRun4_realistic_v1_RV269_Run4D110_PU-v2/2590000/0f0bcfd3-dafe-4dda-8d39-9765f6eae68e.root')
 )
 
 process.maxEvents = cms.untracked.PSet(
-  input = cms.untracked.int32(-1)
+  input = cms.untracked.int32(10)
 )
 
-#################################################################################################
+################################################################################
 # load the analyzer
-#################################################################################################
+################################################################################
 process.AnalyzerClusterStub = cms.EDAnalyzer("AnalyzerClusterStub",
-    DebugMode = cms.bool(True)
+  DebugMode = cms.bool(True),
+  TTClusterInputTag = cms.InputTag("TTClustersFromPhase2TrackerDigis", "ClusterInclusive"),
+  TTStubInputTag = cms.InputTag("TTStubsFromPhase2TrackerDigis", "StubAccepted"),
+  TTClusterAssocInputTag = cms.InputTag("TTClusterAssociatorFromPixelDigis", "ClusterInclusive"),
+  TTStubAssocInputTag = cms.InputTag("TTStubAssociatorFromPixelDigis", "StubAccepted"),
+  TrackingParticleInputTag = cms.InputTag("mix", "MergedTrackTruth"),
+  TrackingVertexInputTag = cms.InputTag("mix", "MergedTrackTruth"),   
 )
 
 #################################################################################################
 # define output file and message logger
 #################################################################################################
 process.TFileService = cms.Service("TFileService",
-#  fileName = cms.string('file:AnalyzerClusterStub_ExtendedPhase2TkBE5D_MuonPU140.root')
-#  fileName = cms.string('file:AnalyzerClusterStub_ExtendedPhase2TkBE5D_Pion.root')
-  fileName = cms.string('file:AnalyzerClusterStub_ExtendedPhase2TkBE5D_Muon.root')
-#  fileName = cms.string('file:AnalyzerClusterStub_ExtendedPhase2TkBE5D_DYTauTau.root')
+    fileName = cms.string('file:AnalyzerClusterStub.root'),
+    closeFileFast = cms.untracked.bool(True)
 )
 
 #################################################################################################
