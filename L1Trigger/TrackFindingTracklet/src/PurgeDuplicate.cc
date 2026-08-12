e4ff83f1f9a0edaff6f2d1167ac9030051fe40e0
Alaa <aabdelh4@vols.utk.edu>
2026-08-12 04:16:31 +0200

    Handling all seeds (displaced and ranked) the same way
    On branch HYBRID_DISPLACED_DR_20_1_0_pre1
    Your branch is up to date with 'source/HYBRID_DISPLACED_DR_20_1_0_pre1'.

--- L1Trigger/TrackFindingTracklet/src/PurgeDuplicate.cc
+++ L1Trigger/TrackFindingTracklet/src/PurgeDuplicate.cc
@@ -169,11 +169,12 @@ void PurgeDuplicate::execute(std::vector<Track>& outputtracks, unsigned int iSec
               // Rank-Informed Guess: L1L2 > L3L4 > L1D1 > L2L3 > L2D1 > D1D2 > L5L6 > D3D4
               // Best Displaced Rank: L2L3L4 >  L4L5L6 > L2D1D2 > L2L3D1
               const unsigned int curSeed = aTrack->seedIndex();
-              static const std::vector<int> ranks{1, 5, 2, 7, 4, 3, 8, 6, 9, 10, 12, 11};  //   L2L3L4>  L4L5L6   > L2D1D2 > L2L3D1};};
+              static const std::vector<int> ranks{
+                  1, 5, 2, 7, 4, 3, 8, 6, 9, 10, 12, 11};  //   L2L3L4>  L4L5L6   > L2D1D2 > L2L3D1};};
               if (curSeed < ranks.size()) {
                 seedRank.push_back(ranks[curSeed]);
-              // } else if (settings_.extended()) {
-              //  seedRank.push_back(9);
+                // } else if (settings_.extended()) {
+                //  seedRank.push_back(9);
               } else {
                 throw cms::Exception("LogError") << __FILE__ << " " << __LINE__ << " Seed type " << curSeed
                                                  << " not found in list, and settings->extended() not set.";
@@ -310,7 +311,7 @@ void PurgeDuplicate::execute(std::vector<Track>& outputtracks, unsigned int iSec
               sortedDupMap[jtrk][itrk] = true;
               // Until extended tracking is optimized, dont set this for extended seeds
               // if (seedRank[seedRankIdx[itrk]] != 9) {
-                sortedMergedTrack[jtrk] = true;
+              sortedMergedTrack[jtrk] = true;
               //}
             }
           }
@@ -323,13 +324,13 @@ void PurgeDuplicate::execute(std::vector<Track>& outputtracks, unsigned int iSec
               int preftrk;
               int rejetrk;
               // if (seedRank[seedRankIdx[itrk]] == 9) {  // extended track seed
-                // COMMENT FROM IAN: The swap here reduces the duplicate
-                // rate for extended tracking by 1/4. Why???
+              // COMMENT FROM IAN: The swap here reduces the duplicate
+              // rate for extended tracking by 1/4. Why???
               //  preftrk = jtrk;
               //  rejetrk = itrk;
               //} else {
-                preftrk = itrk;
-                rejetrk = jtrk;
+              preftrk = itrk;
+              rejetrk = jtrk;
               //}
 
               // If the preffered track is in more than one bin, but not in the proper rinv or phi bin, then mark as true
