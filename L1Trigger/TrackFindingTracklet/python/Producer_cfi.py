d0e6bea815b466e0b7aa63ce4f29e4c0a6586fa7
am2023 <a.mastronikolis23@imperial.ac.uk>
2026-09-08 21:40:01 +0100

    adding 5 par model; tested and works

--- L1Trigger/TrackFindingTracklet/python/Producer_cfi.py
+++ L1Trigger/TrackFindingTracklet/python/Producer_cfi.py
@@ -6,7 +6,7 @@ TrackFindingTrackletProducer_params = cms.PSet (
 
   BDT              = cms.FileInPath( "L1Trigger/TrackFindingTracklet/data/HYBRID_NEW_KF_TQ_XGBOOST_v0.json" ), #
   BDT4ParSim       = cms.FileInPath( "L1Trigger/TrackFindingTracklet/data/HYBRID_SIM_4_TQ_XGBOOST_v0.json"),
-  BDT5ParSim       = cms.FileInPath( "L1Trigger/TrackFindingTracklet/data/HYBRID_SIM_4_TQ_XGBOOST_v0.json"),
+  BDT5ParSim       = cms.FileInPath( "L1Trigger/TrackFindingTracklet/data/HYBRID_SIM_5_TQ_XGBOOST_v0.json"),
   InputTagTTDTC    = cms.InputTag  ( "ProducerDTC", "StubAccepted" ),
   InputTagTracklet = cms.InputTag  ( "l1tTTTracksFromExtendedTrackletEmulation", "Level1TTTracks" ), # Tracklet TTTracks
   InputLabelTFP    = cms.string( "ProducerTQ"      ),                  #
